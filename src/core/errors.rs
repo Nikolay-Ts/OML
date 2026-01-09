e37ee4a913918e9eb2b9e467345c3d3d82220271
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2026-01-09 22:11:21 +0100

    refactored the name error to use the macro

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -1,26 +1,5 @@
 use std::fmt;
 use crate::define_error;
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct NameError {
-    pub message: String
-}
-
-impl NameError {
-    pub fn new(message: String) -> Self {
-        Self {
-            message
-        }
-    }
-}
-
-impl fmt::Display for NameError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-// AND THIS:
-impl std::error::Error for NameError {}
-
+define_error!(NameError, "Name Error:");
 define_error!(ConstError, "Modifier Error: ");
